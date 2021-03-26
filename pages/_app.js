6e5678aefed42fbb360844eba401560e66f96d74
paschalidi <paschalidi@outlook.com>
2021-03-26 14:08:48 +0100

    chore: initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+
 import '../styles/index.css'
+import theme from '../chakra-theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider resetCSS theme={theme}>
+    <ColorModeProvider
+      options={{
+        useSystemColorMode: true,
+      }}
+    >
+      <Component {...pageProps} />
+    </ColorModeProvider>
+  </ChakraProvider>
 }
 
 export default MyApp
