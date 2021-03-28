eea494b96017588878b83ee80fbb9ccdf002d314
paschalidi <paschalidi@outlook.com>
2021-03-28 10:19:49 +0200

    fix: gap me

--- components/cards-grid.js
+++ components/cards-grid.js
@@ -1,3 +1,3 @@
 export default function CardsGrid({ children }) {
-    return <div className="grid grid-cols-1 md:grid-cols-3 md:col-gap-3 lg:col-gap-3 row-gap-4 md:row-gap-4 mb-4">{children}</div>
+    return <div className="grid grid-cols-1 md:grid-cols-3 gap-x-3 gap-y-4">{children}</div>
 }
