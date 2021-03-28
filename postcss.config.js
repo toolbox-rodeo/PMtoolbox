f165d834369db8736dae3dc2cb8a4154022f9a9d
paschalidi <paschalidi@outlook.com>
2021-03-28 10:10:37 +0200

    fix: purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,4 @@
-const purgecss = ['@fullhuman/postcss-purgecss',{
-  content: ["./node_modules/megadraft/dist/**/*.css", "./components/**/*.js", "./pages/**/*.js"],
-
-  defaultExtractor: content => {
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-    return broadMatches.concat(innerMatches)
-  }
-}]
-
+/ postcss.config.js
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
-  ]
-}
+  plugins: ["tailwindcss", "postcss-preset-env"]
+};
