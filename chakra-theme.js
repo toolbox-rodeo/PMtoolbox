d7fb2502b6d5afb8561547e063132fb8f2d9ebb1
paschalidi <paschalidi@outlook.com>
2021-03-28 17:17:37 +0200

    chore: lints again

--- chakra-theme.js
+++ chakra-theme.js
@@ -1,32 +1,32 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `Work Sans` }
+const fonts = { mono: `Work Sans` };
 
 const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+  sm: "40em",
+  md: "52em",
+  lg: "64em",
+  xl: "80em",
+});
 
 const theme = extendTheme({
   colors: {
-    black: '#16161D',
+    black: "#16161D",
     brown: {
       100: "#E2DDD5",
       500: "#8E877B",
-    }
+    },
   },
   textStyles: {
-    sm:{
+    sm: {
       fontSize: "13px",
       fontWeight: "semibold",
     },
-    xs:{
+    xs: {
       fontSize: "12px",
       fontWeight: "semibold",
-    }
+    },
   },
   fonts,
   breakpoints,
@@ -42,6 +42,6 @@ const theme = extendTheme({
       },
     },
   },
-})
+});
 
-export default theme
+export default theme;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
 module.exports = {
-	purge: ["./pages/**/*.{js,jsx,css}", "./components/**/*.{js,jsx,css}"],
-	theme: {
-		screens: {
-			"sm": "640px",
-			"md": "768px",
-			"lg": "1024px",
-			"xl": "1280px",
-			"2xl": "1536px",
-		},
-		extend: {
-			colors: {
-				"accent-0": "#fff",
-				"accent-1": "#FAFAFA",
-				"accent-2": "#EAEAEA",
-				"accent-7": "#333",
-				success: "#0070f3",
-				cyan: "#79FFE1",
-			},
-			spacing: {
-				28: "7rem",
-			},
-			letterSpacing: {
-				tighter: "-.04em",
-			},
-			lineHeight: {
-				tight: 1.2,
-			},
-			fontSize: {
-				"5xl": "2.5rem",
-				"6xl": "2.75rem",
-				"7xl": "4.5rem",
-				"8xl": "6.25rem",
-			},
-			boxShadow: {
-				sm: "0px 1px 0px #E2DDD5",
-			},
-		},
-	},
+  purge: ["./pages/**/*.{js,jsx,css}", "./components/**/*.{js,jsx,css}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        "accent-0": "#fff",
+        "accent-1": "#FAFAFA",
+        "accent-2": "#EAEAEA",
+        "accent-7": "#333",
+        success: "#0070f3",
+        cyan: "#79FFE1",
+      },
+      spacing: {
+        28: "7rem",
+      },
+      letterSpacing: {
+        tighter: "-.04em",
+      },
+      lineHeight: {
+        tight: 1.2,
+      },
+      fontSize: {
+        "5xl": "2.5rem",
+        "6xl": "2.75rem",
+        "7xl": "4.5rem",
+        "8xl": "6.25rem",
+      },
+      boxShadow: {
+        sm: "0px 1px 0px #E2DDD5",
+      },
+    },
+  },
 };
