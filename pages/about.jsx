a7cbb458aeaaced83335f101932c3314797a0d56
Maxra32 <rankmaximilian@gmail.com>
2021-05-21 10:15:19 +0200

    Set about & contact pages + fix header h1

---
+++ pages/about.jsx
@@ -0,0 +1,10 @@
+import { Button, ButtonGroup } from "@chakra-ui/react"
+import Link from 'next/link'
+
+
+function About() {
+  return
+  <div>
+   <Button colorScheme="blue">Contact</Button>
+  </div>
+}
