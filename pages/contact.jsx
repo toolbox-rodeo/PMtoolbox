3dc5b7fd8087c1e2bbac6acab6401ded720c3739
Maxra32 <rankmaximilian@gmail.com>
2021-07-09 22:17:47 +0200

    styled: contacts

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -12,6 +12,13 @@ import {
   Box,
   Text
 } from "@chakra-ui/react";
+import {
+  Formik,
+  Form,
+  Field,
+  useFormikContext,
+  useFormik
+} from 'formik';
 
 
 export default function contact() {
@@ -51,8 +58,20 @@ export default function contact() {
   }
 
 
-  return (
+  function validateInput(value) {
+  let error;
+  if (!value) {
+    error = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    error = 'Invalid email address';
+  }
+  return error;
+}
 
+
+{/*
+
+  return (
     <div className={styles.container}>
 
       <Box align="center">
@@ -65,134 +84,143 @@ export default function contact() {
         <br/><br/>
       </Box>
 
-      <form className={styles.main}>
+      <Formik>
+        <Form className={styles.main}>
 
-        <FormControl id="name" className={styles.inputGroup}>
-          <FormLabel htmlFor='name'>Name
-          </FormLabel>
-          <Input
-          type='text'
-          placeholder="Tell us your name..."
-          onChange={(e)=>{setName(e.target.value)}}
-          name='name'/>
-        </FormControl>
-        <br/>
+          <FormControl
+          id="name"
+          className={styles.inputGroup}>
+            <FormLabel htmlFor='name'>Name</FormLabel>
+            <Input
+            type='text'
+            placeholder="Tell us your name..."
+            onChange={(e)=>{setName(e.target.value)}}
+            name='name'/>
+          </FormControl>
+          <br/>
 
-        <FormControl id="email" className={styles.inputGroup} isRequired>
-          <FormLabel htmlFor='email'>Email
-          </FormLabel>
-          <Input
-          type='email'
-          placeholder="...and your email, so we can get back to you."
-          onChange={(e)=>{setEmail(e.target.value)}}
-          name='email'/>
-          <FormHelperText>We'll never share it🤚</FormHelperText>
-        </FormControl>
-        <br/>
+          <FormControl
+          id="email"
+          className={styles.inputGroup}
+          isRequired>
+            <FormLabel htmlFor='email'>Email</FormLabel>
+            <Input
+            type='email'
+            placeholder="...and your email, so we can get back to you."
+            onChange={(e)=>{setEmail(e.target.value)}}
+            name='email'/>
+            <FormHelperText>We'll never share it🤚</FormHelperText>
+          </FormControl>
+          <br/>
 
-        <FormControl className={styles.inputGroup} isRequired>
-          <FormLabel htmlFor='message'>Message</FormLabel>
-          <Textarea
-          type='text'
-          placeholder="What do you want to share with us?"
-          onChange={(e)=>{setMessage(e.target.value)}}
-          name='message'/>
-        </FormControl>
-        <br/>
+          <FormControl
+          className={styles.inputGroup}
+          isRequired>
+            <FormLabel htmlFor='message'>Message</FormLabel>
+            <Textarea
+            type='text'
+            placeholder="What do you want to share with us?"
+            onChange={(e)=>{setMessage(e.target.value)}}
+            name='message'/>
+          </FormControl>
+          <br/>
 
-        <Box align="center">
-          <Button
-            mt={4}
-            colorScheme="teal"
-            type="submit"
-            onClick={(e)=>{handleSubmit(e)}}>
-            Submit
-          </Button>
-          <br/><br/><br/><br/><br/><br/>
-        </Box>
+          <Box align="center">
+            <Button
+              mt={4}
+              colorScheme="teal"
+              type="submit"
+              onClick={(e)=>{handleSubmit(e)}}>
+              Submit
+            </Button>
+            <br/><br/><br/><br/><br/><br/>
+          </Box>
 
-      </form>
+        </Form>
+      </Formik>
     </div>
-
   )
 }
 
+*/}
 
-{/*
-
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  Input,
-  Button,
-  Textarea,
-  Box
-} from "@chakra-ui/react";
-import {
-  Formik,
-  Form,
-  Field,
-  useFormikContext,
-  useFormik
-} from 'formik';
-import React from 'react'
-
-
-  function validateInput(value) {
-    let error
-    if (!value) {
-      error = "Email is required"
-    } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)
-         ) {
-           error = "Invalid email address"
-         }
-    return error
-  }
 
   return (
-    <Formik>
-      {(props) => (
-        <Box marginLeft="10%" marginRight="10%">
-        <br />
+    <div className={styles.container}>
+      <Box align="center">
+        <br /><br />
+        <Text fontSize="6xl">
+          <h1>
+            CONTACT US
+          </h1>
+        </Text>
+        <br/><br/>
+      </Box>
 
-        <Form onSubmit={formik.handleSubmit}>
-          <Field name="email" validate={validateInput}>
-            {({ field, form }) => (
-              <FormControl id="email" isInvalid={form.errors.email && form.touched.email} isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input {...field} id="email" type="email" placeholder="bruce@wayne.com" maxWidth="700" {...formik.getFieldProps('email')} />
-                <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                <FormHelperText>Email is required to reply. We'll never share it ✋.</FormHelperText>
-              </FormControl>
-            )}
-          </Field>
+      <Formik>
+        {(props) => (
+          <Box margin="auto" width="75%" maxWidth="700px">
           <br />
 
-          <Field name="text">
-            {({ field, form }) => (
-              <FormControl id="text">
-                <FormLabel>Message</FormLabel>
-                <Textarea type="text" placeholder="what do you want to tell us?" maxWidth="700" {...formik.getFieldProps('text')} />
-              </FormControl>
-            )}
-          </Field>
+            <Field name="name">
+              {({ field, form }) => (
+                <FormControl id="name">
+                  <FormLabel>Name</FormLabel>
+                  <Input
+                  type="text"
+                  placeholder="Tell us your name..."
+                  onChange={(e)=>{setName(e.target.value)}}
+                  name='name'/>
+                </FormControl>
+              )}
+            </Field>
 
-          <Button
-            mt={4}
-            colorScheme="teal"
-            isLoading={props.isSubmitting}
-            type="submit">
-            Submit
-          </Button>
-        </Form>
-        <br />
-        </Box>
-      )}
+            <Form>
+              <Field name="email" validate={validateInput}>
+                {({ field, form }) => (
+                  <FormControl id="email" isInvalid={form.errors.email && form.touched.email} isRequired>
+                    <FormLabel>Email</FormLabel>
+                    <Input {...field}
+                    id="email"
+                    type="email"
+                    placeholder="bruce@wayne.com"
+                    onChange={(e)=>{setMessage(e.target.value)}}
+                    name='message' />
+                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                    <FormHelperText>Email is required to reply. We'll never share it ✋.</FormHelperText>
+                  </FormControl>
+                )}
+              </Field>
+              <br />
+
+              <Field name="text">
+                {({ field, form }) => (
+                  <FormControl id="text">
+                    <FormLabel>Message</FormLabel>
+                    <Textarea
+                    type="text"
+                    placeholder="What do you want to share with us?"
+                    onChange={(e)=>{setMessage(e.target.value)}}
+                    name='message'/>
+                  </FormControl>
+                )}
+              </Field>
+
+              <Box align="center">
+                <Button
+                  mt={4}
+                  colorScheme="teal"
+                  isLoading={props.isSubmitting}
+                  type="submit"
+                  onClick={(e)=>{handleSubmit(e)}}>
+                  Submit
+                </Button>
+              </Box>
+            </Form>
+            <br /><br />
+          </Box>
+        )}
       </Formik>
-)}
-
-export default Contact
-*/}
+    </div>
+  )
+}
