08eab17fcfac87a591709730caf9edf1a5efa7b2
Maxra32 <rankmaximilian@gmail.com>
2021-07-26 17:57:10 +0200

    added Duong on about

--- pages/about.jsx
+++ pages/about.jsx
@@ -49,7 +49,7 @@ function About() {
         </Box>
         <Text maxWidth="400" margin="13" fontSize="l">Works on projects with positive impact and decided to work on this project to help teams and organizations improve their processes and therefore their products. He loves eating cherries 🍒</Text>
         <Text align="center" fontSize="xl">Christos</Text>
-        <Text fontSize="l"> <br /> </Text>
+
         <Flex margin="13" justify="center" >
             <a
               href="https://github.com/paschalidi"
@@ -60,6 +60,36 @@ function About() {
             </a>
             <a
               href="https://www.linkedin.com/in/christos-paschalidis/"
+              target="_blank"
+              className="mx-3 hover:underline hover:text-success"
+            >
+              <img src="/linkedin.svg" alt="Linkedin Logo" width={32} />
+            </a>
+        </Flex>
+        </a>
+
+        <a className={styles.card}>
+        <Box align="center">
+        <Image
+          borderRadius="full"
+          boxSize="200px"
+          src="/Max.png"
+          alt="Duong"
+        />
+        </Box>
+        <Text maxWidth="400" margin="13" fontSize="l">Enjoys creating user-centric, delightful, and human experiences. In their free time, they do art, DIYs, and projects like this one.</Text>
+        <Text align="center" fontSize="xl">Duong</Text>
+        <Text fontSize="l"> <br /> <br /> </Text>
+        <Flex margin="13" justify="center" >
+            <a
+              href="https://www.duongpham.design/"
+              target="_blank"
+              className="mx-3 hover:underline hover:text-success"
+            >
+              <img src="/www.svg" alt="Website Logo" width={32} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/dngphm/"
               target="_blank"
               className="mx-3 hover:underline hover:text-success"
             >
@@ -107,35 +137,7 @@ function About() {
           alt="Max"
         />
         </Box>
-        <Text maxWidth="400" margin="13" fontSize="l">Hi! My name's Max. I am a product guy passionate about helping teams to build kick-ass products users love🖤 to use. I spend my days helping incredible teams find and solve problems in health tech. In my spare time I’m also coding on own projects - such as this - although it’s a bit of a stretch to call dirty hacking coding. Feel free to check out my GitHub or contact me on linkedin.</Text>
-        <Flex margin="13" justify="center" >
-            <a
-              href="https://github.com/maxra3209"
-              target="_blank"
-              className="mx-3 hover:underline hover:text-success"
-            >
-              <img src="/github.svg" alt="Github Logo" width={32} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/max-rank/"
-              target="_blank"
-              className="mx-3 hover:underline hover:text-success"
-            >
-              <img src="/linkedin.svg" alt="Linkedin Logo" width={32} />
-            </a>
-        </Flex>
-        </a>
-
-        <a className={styles.card}>
-        <Box align="center">
-        <Image
-          borderRadius="full"
-          boxSize="200px"
-          src="/Max.png"
-          alt="Max"
-        />
-        </Box>
-        <Text maxWidth="400" margin="13" fontSize="l">Hi! My name's Max. I am a product guy passionate about helping teams to build kick-ass products users love🖤 to use. I spend my days helping incredible teams find and solve problems in health tech. In my spare time I’m also coding on own projects - such as this - although it’s a bit of a stretch to call dirty hacking coding. Feel free to check out my GitHub or contact me on linkedin.</Text>
+        <Text maxWidth="400" margin="13" fontSize="l">Hi! My name's Max. I am a product guy passionate about helping teams to build kick-ass products users love🖤 to use. I spend my days helping incredible teams find and solve problems in health tech. In my spare time I’m also coding on own projects - such as this - although it’s a bit of a stretch to call dirty hacking coding.</Text>
         <Flex margin="13" justify="center" >
             <a
               href="https://github.com/maxra3209"
