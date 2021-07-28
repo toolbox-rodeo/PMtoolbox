efcfd8f3ef46355022178a7b495b6e8d0c7e3b6a
christos <paschalidi@outlook.com>
2021-07-28 22:01:08 +0300

    fix: removes revert

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -62,7 +62,6 @@ export default function contact() {
     });
     console.log("Response received");
     console.log(res);
-    debugger;
     if (res.status === 200) {
       console.log("Request succeeded!");
       // Show toast
