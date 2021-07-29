b48f708fcd342b0bb879352b7b2f0d3da08f2fab
christos <paschalidi@outlook.com>
2021-07-29 10:52:40 +0300

    fix: returns promise

--- functions/contact.js
+++ functions/contact.js
@@ -1,18 +1,8 @@
 const nodemailer = require("nodemailer");
 
-exports.handler = async function (event, context, callback) {
-  // Parse the JSON text received.
-  const body = JSON.parse(event.body);
-
-  // Build an HTML string to represent the body of the email to be sent.
-  const html = `<div style="margin: 20px auto;">${body.body}</div>`;
-  console.log("@@@@@@@@@@@@@@");
-  console.log(html);
-  // Generate test SMTP service account from ethereal.email. Only needed if you
-  // don't have a real mail account for testing
+exports.handler = async function contact(event) {
+  const html = `<div style="margin: 20px auto;">${event.body}</div>`;
   const testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
@@ -23,20 +13,28 @@ exports.handler = async function (event, context, callback) {
     },
   });
 
-  try {
-    // send mail with defined transport object
-    const info = await transporter.sendMail({
-      from: '"☁️ The Cloud ☁️" <thecloud@example.com>',
-      to: "paschalidi@outlook.com",
-      subject: "New Form Submission",
-      text: body.body,
-      html,
-    });
-    // Log the result
-    console.log(info);
-    callback(null, { statusCode: 200, body: JSON.stringify(info) });
-  } catch (error) {
-    // Catch and log error.
-    callback(error);
-  }
+  return new Promise((resolve, reject) => {
+    transporter
+      .sendMail({
+        from: '"☁️ The Cloud ☁️" <thecloud@example.com>',
+        to: "paschalidi.chris@gmail.com",
+        subject: "New Form Submission",
+        text: event.body,
+        html,
+      })
+      .then((info) =>
+        resolve({
+          statusCode: 200,
+          body: JSON.stringify({ data: info }),
+        })
+      )
+      .catch((error) =>
+        reject({
+          statusCode: 400,
+          body: JSON.stringify({
+            data: error,
+          }),
+        })
+      );
+  });
 };
