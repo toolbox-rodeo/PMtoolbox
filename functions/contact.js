96205ddf8a80aea28b868e8cbaf98d0c5e9e2839
Maxra32 <rankmaximilian@gmail.com>
2021-08-04 10:33:15 +0200

    try fix contact form

--- functions/contact.js
+++ functions/contact.js
@@ -4,19 +4,19 @@ exports.handler = async function contact(event) {
   const html = `<div style="margin: 20px auto;">${event.body}</div>`;
   const testAccount = await nodemailer.createTestAccount();
   const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
+      user: "becomingdiga@gmail.com", // generated ethereal user
+      pass: "LafaY3Tt3!", // generated ethereal password
     },
   });
 
   return new Promise((resolve, reject) => {
     transporter
       .sendMail({
-        from: '"☁️ The Cloud ☁️" <thecloud@example.com>',
+        from: "becomingdiga@gmail.com",
         to: "hello@rank.health",
         subject: "New Form Submission",
         text: event.body,
